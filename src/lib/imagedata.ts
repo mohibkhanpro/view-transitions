ebdc384ef3b6a1f30ffaae5b94c6d65a8157f2a5
Mohib-Khan-20 <mohibkhan234579@gmail.com>
2024-02-18 14:09:51 -0800

    update

---
+++ src/lib/imagedata.ts
@@ -0,0 +1,30 @@
+// src/data/images.js
+export const imageData = [
+    {
+      id: 1,
+      url: "../../public/favimage.jpg",
+      alt: 'Image 1',
+ 
+    },
+    {
+      id: 2,
+      url: '../../public/favicon.svg',
+      alt: 'Image 2',
+      
+    },
+    
+    {
+      id: 3,
+      url: '/path/to/image3.jpg',
+      alt: 'Image 3',
+    
+    },
+    {
+      id: 4,
+      url: '/path/to/image4.jpg',
+      alt: 'Image 4',
+      
+    },
+  
+  ];
+  
